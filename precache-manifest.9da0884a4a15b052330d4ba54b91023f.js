675a0a47dc4f42d598b1224f8b08d15d2fc7e830
sk_irfan <irfan.shaikh.7652@gmail.com>
2020-04-06 09:55:02 +0530

    Updates

---
+++ precache-manifest.9da0884a4a15b052330d4ba54b91023f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb91dac3254c305050933307838eaf7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5a6525f33242ece9d509",
+    "url": "/monsters-rolodex/static/css/main.1e6610c3.chunk.css"
+  },
+  {
+    "revision": "f3bc58ad93c95ac2dfd4",
+    "url": "/monsters-rolodex/static/js/2.7158d2bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7158d2bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a6525f33242ece9d509",
+    "url": "/monsters-rolodex/static/js/main.f1e96d4a.chunk.js"
+  },
+  {
+    "revision": "5186047b825f1e97286f",
+    "url": "/monsters-rolodex/static/js/runtime-main.34ec292c.js"
+  }
+]);
